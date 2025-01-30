b0c710a9cbd78cab11e7f522bba746bb310a768f
kaoutar-ouadih <kaoutar.ouadih9@gmail.com>
2025-01-30 22:50:29 +0100

    adjust the background

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import './App.css'
 function App() {
 
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <div className='flex flex-col items-center shadow-md rounded-2xl overflow-hidden'>
+    <div className='flex justify-center items-center h-screen bg-[hsl(185,75%,39%)] relative'>
+      <img src="/assets/bg-pattern-top.svg" alt="bg top pattern" className='absolute -top-[40%] -left-[40%]' />
+      <img src="/assets/bg-pattern-bottom.svg" alt="bg bottom pattern" className='absolute -bottom-[40%] -right-[40%]' />
+      <div className='flex flex-col items-center shadow-md rounded-2xl overflow-hidden bg-white z-10'>
         <img src="/assets/bg-pattern-card.svg" alt="background image" />
         <img src="/assets/image-victor.jpg" alt="profile image"  className='rounded-full relative -top-12 w-fit bg-green-400'/>
         <span className='font-bold -mt-9'>Victor Crest   <span className='text-gray-400 ml-2'>26</span></span>
